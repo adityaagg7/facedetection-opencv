21de24c4ffb377ebecba5c62705fe0f3f51f49cb
tida1732 <79642133+tida1732@users.noreply.github.com>
2022-04-13 19:16:39 +0530

    feat: add smile-detection

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ face_Cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_Cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_eye.xml')
+smile_Cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + 'haarcascade_smile.xml')
 
 
 def detect(greyscale, colorframe):
@@ -16,6 +18,10 @@ def detect(greyscale, colorframe):
         roi_color = colorframe[y:y+h, x:x+w]
         eye = eye_Cascade.detectMultiScale(roi_grey, 1.7, 5)
         for (a, b, wi, hi) in eye:
+            cv2.rectangle(roi_color, (a, b), (a+wi, b+hi), (0, 255, 0), 2)
+
+        smile = smile_Cascade.detectMultiScale(roi_grey, 1.7, 5)
+        for (a, b, wi, hi) in smile:
             cv2.rectangle(roi_color, (a, b), (a+wi, b+hi), (0, 255, 0), 2)
 
     return colorframe
